58f63b926fd05b057f55b84dcc6c9125bc685c3a
Alexander Nyquist <nyquist.alexander@gmail.com>
2014-07-03 10:20:06 +0200

    Initial commit

---
+++ HostAdd/Main.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace HostAdd
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.txtDocumentRoot = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtApacheHostFile = new System.Windows.Forms.TextBox();
+            this.btnGogogo = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtHostname = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtDocumentRoot);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.txtApacheHostFile);
+            this.groupBox1.Controls.Add(this.btnGogogo);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtHostname);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(8, 7);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(462, 189);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Add new host";
+            // 
+            // txtDocumentRoot
+            // 
+            this.txtDocumentRoot.Location = new System.Drawing.Point(9, 81);
+            this.txtDocumentRoot.Name = "txtDocumentRoot";
+            this.txtDocumentRoot.Size = new System.Drawing.Size(444, 20);
+            this.txtDocumentRoot.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 65);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Document root";
+            // 
+            // txtApacheHostFile
+            // 
+            this.txtApacheHostFile.Location = new System.Drawing.Point(9, 129);
+            this.txtApacheHostFile.Name = "txtApacheHostFile";
+            this.txtApacheHostFile.Size = new System.Drawing.Size(441, 20);
+            this.txtApacheHostFile.TabIndex = 3;
+            // 
+            // btnGogogo
+            // 
+            this.btnGogogo.Location = new System.Drawing.Point(375, 155);
+            this.btnGogogo.Name = "btnGogogo";
+            this.btnGogogo.Size = new System.Drawing.Size(75, 23);
+            this.btnGogogo.TabIndex = 4;
+            this.btnGogogo.Text = "Add";
+            this.btnGogogo.UseVisualStyleBackColor = true;
+            this.btnGogogo.Click += new System.EventHandler(this.btnGogogo_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 113);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(151, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Path to Apache VirtualHost-file";
+            // 
+            // txtHostname
+            // 
+            this.txtHostname.Location = new System.Drawing.Point(9, 32);
+            this.txtHostname.Name = "txtHostname";
+            this.txtHostname.Size = new System.Drawing.Size(441, 20);
+            this.txtHostname.TabIndex = 1;
+            this.txtHostname.Text = "test.localhost";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 16);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(55, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Hostname";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(481, 207);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Main";
+            this.Text = "Add new host";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtHostname;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnGogogo;
+        private System.Windows.Forms.TextBox txtApacheHostFile;
+        private System.Windows.Forms.TextBox txtDocumentRoot;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ HostAdd/Main.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace HostAdd
+{
+    public partial class Main : Form
+    {
+        private readonly string _vhostTemplate = Environment.NewLine + Environment.NewLine + @"<VirtualHost *:80>
+    ServerAdmin webmaster@dummy-host2.example.com
+    DocumentRoot ""{0}""
+    ServerName {1}
+    ErrorLog ""logs/dummy-host2.example.com-error.log""
+    CustomLog ""logs/dummy-host2.example.com-access.log"" common
+</VirtualHost>";
+
+        public Main()
+        {
+            InitializeComponent();
+
+            // Try to guess path to VHost-file
+            if (Directory.Exists(@"C:\wamp") && Directory.Exists(@"C:\wamp\bin\apache"))
+            {
+                var apacheVersions = Directory.GetDirectories(@"C:\wamp\bin\apache");
+                if (apacheVersions.Any())
+                {
+                    var directoryInfo = new DirectoryInfo(apacheVersions[0]);
+                    txtApacheHostFile.Text = string.Format(directoryInfo.FullName + @"\conf\extra\httpd-vhosts.conf");
+                }
+            }
+        }
+
+        private void btnGogogo_Click(object sender, EventArgs e)
+        {
+            if (!ValidatePaths())
+            {
+                MessageBox.Show("All path are required to be filled in.", "HostAdd", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+                return;
+            }
+
+            var backupTime = DateTime.Now.ToString().Replace(":", "-");
+
+            if (!File.Exists(txtApacheHostFile.Text))
+            {
+                FailExit("Apache v-host file not found");
+            }
+
+            // Make a backup
+            var apacheHostFile = new FileInfo(txtApacheHostFile.Text);
+            if (!Directory.Exists("backups"))
+            {
+                Directory.CreateDirectory("backups");
+            }
+
+            apacheHostFile.CopyTo(string.Format("backups/{0}-{1}", backupTime, apacheHostFile.Name));
+
+            // Validate
+            if (File.ReadAllText(apacheHostFile.FullName).Contains(txtHostname.Text))
+            {
+                if(!Confirm(txtHostname.Text + " already exists in v-host file. Continue anyway?"))
+                {
+                    Environment.Exit(0);   
+                }
+            }
+
+            // Add new host
+            var template = string.Format(_vhostTemplate, txtDocumentRoot.Text, txtHostname.Text);
+            File.AppendAllText(apacheHostFile.FullName, template);
+
+            // Add host to /etc/hosts
+            var hostsFile = new FileInfo(@"C:\Windows\System32\drivers\etc\hosts");
+            if (!hostsFile.Exists)
+            {
+                FailExit(@"Could not find hosts file C:\Windows\System32\drivers\etc\hosts! Broken system?");
+            }
+
+            // Backup hosts file
+            hostsFile.CopyTo(string.Format("backups/{0}-hosts", backupTime));
+
+            // Validate
+            if (File.ReadAllText(hostsFile.FullName).Contains(txtHostname.Text))
+            {
+                if (!Confirm(txtHostname.Text + " already exists in hosts file. Continue anyway?"))
+                {
+                    Environment.Exit(0);
+                }
+            }
+
+            // Add host
+            File.AppendAllText(hostsFile.FullName, string.Format("\n{0}\t{1}", "127.0.0.1", txtHostname.Text));
+
+            // Create document root folder if not exists
+            if (!Directory.Exists(txtDocumentRoot.Text))
+            {
+                if (Confirm(string.Format("Document root folder ({0}) does not exist. Create?", txtDocumentRoot.Text)))
+                {
+                    Directory.CreateDirectory(txtDocumentRoot.Text);
+                }
+            }
+
+            // Done!
+            var openHost = MessageBox.Show(string.Format("Done! Remember to restart Apache. Go to http://{0}?", txtHostname.Text), "HostAdd",
+                            MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (openHost == DialogResult.Yes)
+            {
+                Process.Start("http://" + txtHostname.Text + "/");
+            }
+
+            Environment.Exit(0);
+        }
+
+        private void FailExit(string message)
+        {
+            MessageBox.Show(message, "HostAdd", MessageBoxButtons.OK, MessageBoxIcon.Exclamation);
+            Environment.Exit(1);
+        }
+
+        private static bool Confirm(string question)
+        {
+            var confirm = MessageBox.Show(question, "HostAdd", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            return confirm == DialogResult.Yes;
+        }
+
+        private bool ValidatePaths()
+        {
+            return !string.IsNullOrWhiteSpace(txtHostname.Text) &&
+                   !string.IsNullOrWhiteSpace(txtDocumentRoot.Text) &&
+                   !string.IsNullOrWhiteSpace(txtApacheHostFile.Text);
+        }
+    }
+}
